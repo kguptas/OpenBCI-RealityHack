72cb796533af8949d04e25f786159263d4e76d8e
Peter He <ph475@cornell.edu>
2025-01-26 05:29:25 -0500

    data streaming to hardware

--- Assets/Scripts/AlphaTest.cs
+++ Assets/Scripts/AlphaTest.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
-using OpenBCI.Network.Streams; // make sure you import the right thing from the OpenBCI SDK package
+using OpenBCI.Network.Streams;
+using System.Data; // make sure you import the right thing from the OpenBCI SDK package
 
 public class AlphaTest : MonoBehaviour
 {
     public GameObject pillar;  // make sure you have a GameObject in the scene to represent the pillar
-    [SerializeField] private AverageBandPowerStream Stream; // attach the AverageBandPowerStream from the Unity prefab
-    [SerializeField] private PPGMetricsStream HeartRateStream; // attach the HeartRateStream from the Unity prefab
-    [SerializeField] private EMGStream FaceStream;
+    [SerializeField] private EMGStream EMG; // attach the HeartRateStream from the Unity prefab
     private float pillarHeight;
-    float currentRatio = 0;
+    float sum = 0;
     float previousRatio = 0;
-    float changeThreshold = 0.3f;
+    float changeThresholdEMG = 0.7f;
     int frames = 0;
     // Start is called once before the first frame update
+
+
     void Start()
     {
         pillarHeight = 0f;
@@ -21,53 +22,26 @@ public class AlphaTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // log the alpha band power value to the unity console
-        //if statement for console logs to happen every 100 frames
-        if (frames % 60 == 0)
+
+        if (frames % 220 == 0)
         {
-            // Debug.Log("Alpha: " + Stream.AverageBandPower.Alpha);
-            // Debug.Log("Beta: " + Stream.AverageBandPower.Beta);
-            //print out all of heart rate data
-            for (int i = 0; i < HeartRateStream.GetHeartRateData().Length; i++)
-            {
-                Debug.Log("Heart Rate: " + HeartRateStream.GetHeartRateData()[i]);
+            float[] data = EMG.Channels;
+            sum = data[0]+data[1]+data[2]+data[3]+data[6]+data[7];
+            sum = sum/6;
+            // Debug.Log("EMG CH1 left cheek: " + data[0]);
+            // Debug.Log("EMG CH2 right cheek: " + data[1]);
+            // Debug.Log("EMG CH3 left eyebrow: " + data[2]);
+            // Debug.Log("EMG CH4 right eyebrow: " + data[3]);
+            // Debug.Log("EMG CH7 left forehead: " + data[6]);
+            // Debug.Log("EMG CH8 right forehead: " + data[7]);
+            Debug.Log("EMG AVE: " + sum);
+            if(sum >= changeThresholdEMG){
+                //trigger
             }
-            //Debug.Log("Heart Rate: " + HeartRateStream.GetHeartRateData()[0]);
-            frames = 0;
 
         }
+        frames++;
        
-        // float alpha = Stream.AverageBandPower.Alpha;
-        // float beta = Stream.AverageBandPower.Beta;
-
-        // // Debug.Log("Alpha: " + Stream.AverageBandPower.Alpha); //make alpha focus thresholds
-        // // Debug.Log("Beta: " + Stream.AverageBandPower.Beta); //make beta stress thresholds
-        // Debug.Log("Heart Rate: " + HeartRateStream.GetHeartRateData()[0]);
-
-        //  if (beta != 0f) // Avoid division by zero
-        // {
-        //     currentRatio = alpha / beta;
-        // }
-        // else
-        // {
-        //     currentRatio = Mathf.Infinity; // Or handle this case as needed
-        // }
-
-        // // Check if the ratio has changed significantly
-        // if (Mathf.Abs(currentRatio - previousRatio) > changeThreshold)
-        // {
-        //     Debug.Log("Ratio has changed significantly");
-        //     previousRatio = currentRatio; // Update previous ratio
-        // }
-
-        // for (int i = 0; i < FaceStream.Channels.Length; i++)
-        // {
-        //     Debug.Log("EMG Channel " + i + ": " + FaceStream.Channels[i]);
-        // }
-
-        // // scale the cylinder height based on the alpha band power
-        // pillarHeight = Stream.AverageBandPower.Alpha;
-        // pillar.transform.localScale = new Vector3(1, pillarHeight, 1); 
     }
 
     
